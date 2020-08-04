6c54c4b97afdf2b9c14f45d76c525d920cf0e523
kanijfatemaritu <67449764+kanijfatemaritu@users.noreply.github.com>
2020-08-04 17:39:56 +0600

    added leospa-testimonial-carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    $('.owl-carousel').owlCarousel({
+    $('.leospa-feature-carousel').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
@@ -12,6 +12,22 @@ $( document ).ready(function() {
             },
             1000:{
                 items:4
+            }
+        }
+    })
+    $('.leospa-testimonial-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
             }
         }
     })
